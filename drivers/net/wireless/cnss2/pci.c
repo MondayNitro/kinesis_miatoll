41bf97519e288a0997c8c8f34075f522fdda8479
Vaisakh Murali <mvaisakh@statixos.com>
2025-12-05 18:46:08 +0000

    cnss2: switch to late suspend irq instead of noirq suspend
    noirq suspend irq leads to callback fails, waking up the CPU from
    suspend.
    
    Co-authored-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I00541c5ec35a61b9725092f5a60d134b97291bf5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    (cherry picked from commit 0a42ff28d05de22d7898e21e6dc5216e93ae0fbf)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- drivers/net/wireless/cnss2/pci.c
+++ drivers/net/wireless/cnss2/pci.c
@@ -1747,7 +1747,7 @@ out:
 	return ret;
 }
 
-static int cnss_pci_suspend_noirq(struct device *dev)
+static int cnss_pci_suspend_late(struct device *dev)
 {
 	int ret = 0;
 	struct pci_dev *pci_dev = to_pci_dev(dev);
@@ -1758,8 +1758,8 @@ static int cnss_pci_suspend_noirq(struct device *dev)
 		goto out;
 
 	driver_ops = pci_priv->driver_ops;
-	if (driver_ops && driver_ops->suspend_noirq)
-		ret = driver_ops->suspend_noirq(pci_dev);
+	if (driver_ops && driver_ops->suspend_late)
+		ret = driver_ops->suspend_late(pci_dev);
 
 	if (pci_priv->disable_pc && !pci_dev->state_saved)
 		pci_save_state(pci_dev);
@@ -1768,7 +1768,7 @@ out:
 	return ret;
 }
 
-static int cnss_pci_resume_noirq(struct device *dev)
+static int cnss_pci_resume_late(struct device *dev)
 {
 	int ret = 0;
 	struct pci_dev *pci_dev = to_pci_dev(dev);
@@ -1779,9 +1779,9 @@ static int cnss_pci_resume_noirq(struct device *dev)
 		goto out;
 
 	driver_ops = pci_priv->driver_ops;
-	if (driver_ops && driver_ops->resume_noirq &&
+	if (driver_ops && driver_ops->resume_late &&
 	    !pci_priv->pci_link_down_ind)
-		ret = driver_ops->resume_noirq(pci_dev);
+		ret = driver_ops->resume_late(pci_dev);
 
 out:
 	return ret;
@@ -3773,8 +3773,8 @@ MODULE_DEVICE_TABLE(pci, cnss_pci_id_table);
 
 static const struct dev_pm_ops cnss_pm_ops = {
 	SET_SYSTEM_SLEEP_PM_OPS(cnss_pci_suspend, cnss_pci_resume)
-	SET_NOIRQ_SYSTEM_SLEEP_PM_OPS(cnss_pci_suspend_noirq,
-				      cnss_pci_resume_noirq)
+	SET_LATE_SYSTEM_SLEEP_PM_OPS(cnss_pci_suspend_late,
+				      cnss_pci_resume_late)
 	SET_RUNTIME_PM_OPS(cnss_pci_runtime_suspend, cnss_pci_runtime_resume,
 			   cnss_pci_runtime_idle)
 };
